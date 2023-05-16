ab337bc5c14725d3c2645588f99ca4bbeae9ea57
ChristianEtsey98 <72524355+ChristianEtsey98@users.noreply.github.com>
2023-05-16 11:20:01 +0200

    Update ImageTransformer.cpp
    Updated version of LightenDarken

--- Classes/ImageTransformer.cpp
+++ Classes/ImageTransformer.cpp
@@ -34,64 +34,20 @@ Mat ImageTransformer::canny(Mat& img, double lowThreshold, double highThreshold,
 
 }
 
-int bright_contr_students (){
-    
-    double alpha;
-    string imagePath;
-    bool imageEmpty=false;
-    Mat image;
+Mat ImageTransformer::LightenDarken (Mat& image, double lightIntensity){
     
 
-    //reading the image and creating a new image matrix
-    
-    
-    do {
-        
-        cout << "Enter the path to the image: ";
-        cin >> imagePath;
-        image= imread(imagePath);
-        
-        if (image.empty() || image.empty())
-            {
-                cout << "Could not open or find the images" << endl;
-                imageEmpty=false;
-            }
-        else imageEmpty=true;
-    }
-    while (imageEmpty==false);
-    
+    //creating a new image matrix
+
     Mat lightenDarken_image = Mat::zeros(image.size(), image.type());
     
-    //asking for alpha [1.0-3.0] and beta values [0-100]
-    
-    do {
-        cout << "Enter alpha value [-100 and 100]: ";
-        cin >> alpha;
-    }
-    while ( alpha < -100 || alpha > 100);
-    
-    
-        
     for(int y = 0; y < image.rows; y++) {
         for(int x = 0; x < image.cols; x++) {
             for(int c = 0; c < image.channels(); c++) {
                 lightenDarken_image.at<Vec3b>(y,x)[c] =
-                  saturate_cast<uchar>(( image.at<Vec3b>(y,x)[c] ) + alpha );
+                  saturate_cast<uchar>(( image.at<Vec3b>(y,x)[c] ) + lightIntensity );
            }
         }
     }
-    
-    //create windows
-    
-    namedWindow("Original Image", WINDOW_NORMAL);
-    namedWindow("New Image", WINDOW_NORMAL);
-    
-    //show original and new image
- 
-    imshow("Original Image", image);
-    imshow("New Image", lightenDarken_image);
-    
-    
-    waitKey();
-    return 0;
+    return lightenDarken_image;
 }
